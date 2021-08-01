47bf22b63fc5f5adcbc215ef7a4d8ba6eb311e61
amin-nassar <amin.nassar@stu.najah.edu>
2021-08-01 12:50:52 +0300

    Array - Filter - Implementation

--- js/arrayUtils.js
+++ js/arrayUtils.js
@@ -12,4 +12,17 @@ function map(array, callback, thisArg) {
   return outputArray;
 }
 
-module.exports = { map };
+function filter(array, callback, thisArg) {
+  if (!Array.isArray(array)) throw new TypeError(`${array} Is Not An Array`);
+  if (!(callback instanceof Function))
+    throw new TypeError(`${callback} Is Not A Function`);
+  const outputArray = [];
+  const boundCallBack = callback.bind(thisArg);
+  for (let index = 0; index < array.length; index++) {
+    const item = array[index];
+    if (boundCallBack(item, index, array)) outputArray.push(item);
+  }
+  return outputArray;
+}
+
+module.exports = { map, filter };
